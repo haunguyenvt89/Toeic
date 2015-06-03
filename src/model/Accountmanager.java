ceaa620be94de9cfacc779f2f6dc43f6e3bd56c5
hau nguyen <haunguyenvt89@gmail.com>
2015-06-03 07:30:49 +0700

    Login

--- src/model/Accountmanager.java
+++ src/model/Accountmanager.java
@@ -1,18 +1,33 @@
 package model;
 
 import java.awt.List;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import pojo.User;
 public class Accountmanager {
 	DatabaseManager db;
-	public boolean checkLogin(String username, String password)
+	public int	checkLogin(String username, String password) throws SQLException
 	{
-		return true;
+		db = new DatabaseManager();
+		db.open();
+		String sql = "Select * from nguoidung Where username = '" + username + "' and password = '" + password + "'";
+		ResultSet rs = db.query(sql);
+		if (rs.next()) {
+			int kq = rs.getInt("loaiuser");
+			if(kq== 1)
+				return 1;
+			else
+				if(kq == 2)
+					return 2;
+		}
+		return 0;
 	}
 	public ArrayList<User> getUser()
 	{
 		ArrayList<User> lstuser = null;
 		return lstuser;
 	}
+	
 }
